294decf0e36652aecb1a89ac73389fcf194b4ecd
Alumno <samuel2004romero@gmail.com>
2023-03-29 11:17:41 -0500

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-asdsad
+var http = require('http');
+
+var manejador  = function(solicitud,respuesta){
+    respuesta.end('Hola Mundo!');
+};
+
+var servidor = http.createServer(manejador);
+
+servidor.listen(3000);
